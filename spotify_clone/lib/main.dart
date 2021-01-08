caae0dfb6ff4f1c2fbdc60705bedd3d24f3af215
notakbuzz <74129681+notakbuzz@users.noreply.github.com>
2021-01-08 16:03:04 +0530

    Update main.dart

--- spotify_clone/lib/main.dart
+++ spotify_clone/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Spotify Clone',
+      title: 'Spotify Recommendation Engine',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
